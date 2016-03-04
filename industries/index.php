7977ce41724de5d2454deb7f042007ea5e91579f
windowsazure <windowsazure>
2016-03-04 08:02:51 +0000

    Industries page

---
+++ industries/index.php
@@ -0,0 +1,103 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/doctype.php");?>
+    <title>Bloom - Industries</title>
+    <meta name="description" content="">
+    <meta name="author" content="Bloom">
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/styles.php");?>
+
+<body class="activate-appear-animation header-6">
+    <!-- global-wrapper -->
+    <div id="global-wrapper">
+        
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/navigation.php");?>
+        <!-- content-->
+
+        <main id="content">
+            <header class="page-header center x-large image-background no-padding rel" style="background-image: url('/images/background-industries.jpg');">
+            </header>
+
+            <!-- page content -->
+
+            <section class="pt-medium light-color firstblock">
+                <div class="container">
+                    <div class="row">
+                            <div class="col-xs-3 col-xxs-12 text-center">
+                                <img src="/images/logo-bloom-security-color-gray.png" class="img-responsive client-logo">
+                            </div>
+                            <div class="col-xs-9 col-xxs-12">
+                                <p class="text0000 light">Private & Enterprise Security</p>
+                                <p class="text0 regular">
+                                    The private security industry is one of Bloom's primary focus areas for solutions and services. Bloom has deep experience working with organizations that provide a wide range of services, including guard services, alarm monitoring, investigation, armored transport, systems integration and management, security consulting, and information technology security. Bloom works mostly with commercial and critical infrastructure organizations to modernize the business of security to better protect employees and property, conduct investigations, predict threats, and respond to incidents.
+                                </p>
+                            </div>
+                        </div>
+                    </div> <!-- / row -->
+                </div><!-- / container -->
+            </section>
+
+            <section class="pt-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="text000 light">High Tech</p>
+                            <p class="text regular">
+                                Bloom consultants have been working for over twenty years in high tech with executives that are responsible marketing and selling the most advanced technology direct and indirect through complex partner channels. Bloom is a true partner with marketing, sales, and channel executives in high tech to solve complex global business challenges and realize business transformation.
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="text000 light">Oil & Gas</p>
+                            <p class="text regular">
+                                Visualizing dashboards to display industrial control system data means aligning the layout in a way that is generally consistent with the operator’s mental model of the process. Important information and key performance indicators have embedded trends and bright colors are used only to quickly draw the operator’s attention to abnormal colors and alarms. These are some of the key considerations when designing world class visualization for Oil & Gas Operators and executives. Bloom understand this industry and can harness big data to provide important insight.
+                            </p>
+                        </div>
+                    </div> <!-- / row -->
+                </div><!-- / container -->
+            </section>
+            <section class="pt-small light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="text000 light">Entertainment</p>
+                            <p class="text regular">
+                                Changing technology and consumer engagement habits present some of the biggest challenges for media and entertainment today. Consumers switch between multiple devices throughout the day, dictating where, when and how they will watch movies, TV shows, listen to music, or catch up on the day's events. Bloom can help Entertainment companies reach their audiences more effectively through follow-you-anywhere device experiences.
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="text000 light">Other Industries</p>
+                            <p class="text regular">
+                                All industries aim to increase productivity, cut costs, and add value to their solutions. Bloom’s full range of services and solutions, including big data analytics, performance improvement, change management, business intelligence, and application development services, support executives in all industries. Bloom’s ability to help companies modernize their employee and customer experiences across devices makes us one of the move innovative and progressive consulting and technologies companies in the world today.
+                            </p>
+                        </div>
+                    </div> <!-- / row -->
+                </div><!-- / container -->
+            </section>
+            
+            <section class="pt-medium">
+                    <div class="row">
+                        <div class="col-sm-4 image-background blockphotos" style="background-image: url(/images/industry-video.jpg);">
+                        </div>
+                        <div class="col-sm-4 image-background blockphotos" style="background-image: url(/images/industry-gas.jpg);">
+                        </div>
+                        <div class="col-sm-4 image-background blockphotos" style="background-image: url(/images/industry-shipping.jpg);">
+                        </div>
+
+                    </div> <!-- / row -->
+            </section>
+            
+        </main>
+        <!-- / content -->
+
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.php");?>
+    </div>
+    <!-- global wrapper -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/scripts.php");?>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#page-industries').addClass('active');
+        });
+    </script>
+</body>
+
+</html>

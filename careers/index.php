92dfe076aa56d3724dcb71c78df9f9612d093ce4
windowsazure <windowsazure>
2016-03-04 10:08:29 +0000

    Careers page

---
+++ careers/index.php
@@ -0,0 +1,100 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/doctype.php");?>
+    <title>Bloom - Careers</title>
+    <meta name="description" content="">
+    <meta name="author" content="Bloom">
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/styles.php");?>
+
+<body class="activate-appear-animation header-6">
+    <!-- global-wrapper -->
+    <div id="global-wrapper">
+        
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/navigation.php");?>
+        <!-- content-->
+
+        <main id="content">
+            <header class="page-header center x-large stay image-background no-padding rel" style="background-image: url('/images/background-careers.jpg');">
+
+                <div class="row">
+                    <div class="header-blurb blurb-services text00 light col-md-4">
+						<div class="text0">Employee Testimonials</div>
+                        "One of the best things about working for Bloom is that they encourage creativity and innovation, so you can put your own spin on things while constantly being challenged to do better"
+						<br><br>
+
+						- Cassie Fischer, Project Manager
+                    </div>
+                </div>
+            </header>
+            <!-- page content -->
+            <section class="pt-medium light-color">
+                <div class="container">
+                    <div class="row text0">
+						<p class="text0000 light">Open positions</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="pt-small pb-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p class="text000 light">Windows Developer</p>
+                            <p class="text0 regular">
+                                Skilled software developer passionate about highly interactive multi-tiered mobile applications. Windows store and windows phone experience a plus. Thrives in a fast-paced environment and able to manage ambiguity. Motivated self-starter who brings solutions to the table. Comfortable in a collaborative team environment.
+                            </p>
+                        </div>
+                        <div class="col-md-4 pad-top-75 text-center">
+                            <a class="btn default large block text0000">apply now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="pb-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p class="text000 light">Cloud/Enterprise Solution Architect</p>
+                            <p class="text0 regular">
+                                The Solution Architect is a key member of the company’s custom solution implementation project team and is responsible for the overall design of the application and mapping client business requirements to systems/technical requirements. The Solution Architect works as a team member along with the Customer Representative, Product Owner and supporting team members to deliver the complete solution for the customer. This is a hands-on position.
+                            </p>
+                        </div>
+                        <div class="col-md-4 pad-top-75 text-center">
+                            <a class="btn default large block text0000">apply now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="pb-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p class="text000 light">Database Admin/Business Analyst</p>
+                            <p class="text0 regular">
+                                The Database Admin/BusinessAnalyst will be responsible for the maintenance of existing databases, the design, and implementation of new databases. This person will also be responsible for analyzing our current systems, resolving data reporting issues and making necessary improvements. We are looking for someone that can work with our existing developers and key team members to understand needs and expectations and offer solutions.
+                            </p>
+                        </div>
+                        <div class="col-md-4 pad-top-75 text-center">
+                            <a class="btn default large block text0000">apply now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
+        <!-- / content -->
+
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.php");?>
+    </div>
+    <!-- global wrapper -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/scripts.php");?>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#page-careers').addClass('active');
+        });
+    </script>
+</body>
+
+</html>

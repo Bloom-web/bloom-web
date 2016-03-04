ab9e1cf56c27c12e151f21918ba3b1703bfddbfd
windowsazure <windowsazure>
2016-03-04 09:21:34 +0000

    Innovators page

---
+++ innovators/index.php
@@ -0,0 +1,179 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/doctype.php");?>
+    <title>Bloom - Innovators</title>
+    <meta name="description" content="">
+    <meta name="author" content="Bloom">
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/styles.php");?>
+
+<body class="activate-appear-animation header-6">
+    <!-- global-wrapper -->
+    <div id="global-wrapper">
+        
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/navigation.php");?>
+        <!-- content-->
+
+        <main id="content">
+            <header class="page-header center xlarge image-background no-padding rel" style="background-image: url('/images/background-innovators.jpg');">
+            </header>
+            <!-- page content -->
+
+            <section class="pt-medium pb-small light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p class="text0000 light">Meet the experts</p>
+                            <div class="text-center">
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-bryan.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Bryan Zembureski<br>
+                                        <span class="text-00">
+                                            Consultant
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-cassie.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Cassie Fischer<br>
+                                        <span class="text-00">
+                                            Project Manager
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-eddie.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Eddie Mulcahy<br>
+                                        <span class="text-00">
+                                            Project Manager
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-hoby.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Hoby Van Hoose<br>
+                                        <span class="text-00">
+                                            Experience Architect, Product Owner
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-john.jpg" class="responsive">
+                                    <p class="text regular">
+                                        John Brooks<br>
+                                        <span class="text-00">
+                                            Consultant
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-mason.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Mason Tejera<br>
+                                        <span class="text-00">
+                                            Developer
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-rachelle.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Rachelle Raymond<br>
+                                        <span class="text-00">
+                                            Project Manager
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-sukhie.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Sukhie Patel<br>
+                                        <span class="text-00">
+                                            Project Manager
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-tj.jpg" class="responsive">
+                                    <p class="text regular">
+                                        TJ Filley<br>
+                                        <span class="text-00">
+                                            Consultant
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="col-md-3 col-xs-6">
+                                    <img src="/images/headshot-tracey.jpg" class="responsive">
+                                    <p class="text regular">
+                                        Tracey Cummings<br>
+                                        <span class="text-00">
+                                            Consultant
+                                        </span>
+                                    </p>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- row -->
+                </div>
+                <!-- container -->
+            </section>
+            <section class="pt-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+						  <p class="text0000 light">Featured articles</p>
+                        </div>
+                        <div class="col-xs-4 col-xxs-12 text-center">
+                            <img src="/images/article-smartwatch.jpg" class="img-responsive" style="border:solid 1px black;">
+                        </div>
+                        <div class="col-xs-4 col-xxs-12 text-center">
+                            <img src="/images/article-trends.jpg" class="img-responsive" style="border:solid 1px black;">
+                        </div>
+                        <div class="col-xs-4 col-xxs-12 text-center">
+                            <img src="/images/article-mobility.jpg" class="img-responsive" style="border:solid 1px black;">
+                        </div>
+                    </div> <!-- / row -->
+                </div><!-- / container -->
+            </section>
+
+            <section class="pt-medium pb-medium light-color">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <p class="text0000 light">Awards</p>
+                            <p class="text0 regular">
+                                Bloom is the proud co-winner with Microsoft of the Security Technology Executive Magazine’s Security Innovation Award for best collaborative effort for 2015. Bloom developed an innovative, new Azure-driven Windows Application for the Microsoft Advanced Patrol Platform (MAPP), a joint effort between Microsoft Global Security, Worldwide Solutions, OEM, Worldwide Public Safety and Justice as well as a consortium of leading technology and service partners. 
+                                <br><br>
+                                MAPP is designed to demonstrate the future of mobile law enforcement by streamlining the roles of law enforcement and security professionals through interaction with technology. It is currently demonstrated on a Ford Police Interceptor SUV branded to represent Microsoft technologies and partners, with the goal of providing patrol teams with a mobile command center that delivers critical information and intelligence once only accessible at a physical command center.
+                            </p>
+                        </div>
+                        <div class="col-md-5 pad-top-75 text-center">
+                            <img src="/images/award-security-exec.jpg" class="responsive">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+        </main>
+        <!-- / content -->
+
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.php");?>
+    </div>
+    <!-- global wrapper -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/scripts.php");?>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#page-innovators').addClass('active');
+        });
+    </script>
+</body>
+
+</html>

90e6d5d1de24ced64bfb7d95a4b93c3eb84b2cf0
windowsazure <windowsazure>
2016-03-04 10:47:01 +0000

    Contact page

---
+++ contact/index.php
@@ -0,0 +1,60 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/doctype.php");?>
+    <title>Bloom - Contact</title>
+    <meta name="description" content="">
+    <meta name="author" content="Bloom">
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/styles.php");?>
+
+<body class="activate-appear-animation header-6">
+    <!-- global-wrapper -->
+    <div id="global-wrapper">
+        
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/navigation.php");?>
+        <!-- content-->
+
+        <main id="content">
+            <header class="page-header center xlarge stay image-background no-padding rel" style="background-image: url('/images/background-contact.jpg');">
+            </header>
+            <!-- page content -->
+            <section class="">
+                <div class="">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div><iframe width="100%" height="400" frameborder="0" scrolling="no" seamless="seamless" src="https://www.bing.com/maps/embed/viewer.aspx?v=3&amp;cp=47.679401~-122.099217&amp;lvl=12&amp;w=800&amp;h=400&amp;sty=r&amp;typ=d&amp;pp=&amp;ps=&amp;dir=0&amp;mkt=en-us&amp;src=SHELL&amp;form=BMEMJS"></iframe></div>
+                        </div>
+                        <div class="col-sm-6 pad-top-50">
+                            <div class="text00">Contact us</div>
+                            <br>
+                            1-425-219-3518<br>
+                            information@bloomconsultinggroup.com<br>
+                            <br>
+                            8060 - 165th Ave. NE, Suite 200, Redmond, WA 98052<br>
+    						<br><br>
+    
+    						Offices:<br>
+                            Headquarters and Software Engineering - Redmond, WA (Suite 200-220)<br>
+                            Sales Offices - Redmond, WA + Washington D.C.<br>
+                            <br>
+                            Download a Bloom Consulting Group Brochure
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+        <!-- / content -->
+
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.php");?>
+    </div>
+    <!-- global wrapper -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/scripts.php");?>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#page-contact').addClass('active');
+        });
+    </script>
+</body>
+
+</html>

a2a99d51f1eff9efcbb5f77ef6cd86247fd393b6
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-03-19 01:23:59 +0000

    Corporate bios using show hide toggle

--- corporate/page.php
+++ corporate/page.php
@@ -52,7 +52,7 @@
                         <div class="col-md-12"><a name="global" class="anchor"></a>
                             <p class="text000 light">Global Management</p>
                             <div class="text-center">
-                                <div class="col-md-3 col-sm-6">
+                                <div class="col-md-3 col-sm-6 iblock-multi">
                                     <img src="/images/headshot-nicole.jpg" class="responsive">
                                     <p class="text regular">
                                         Nicole Watson<br>
@@ -60,8 +60,25 @@
                                             Vice President, Strategy
                                         </span>
                                     </p>
+                                	<a class="bio-showhide noselect">bio</a>
+                                	<div class="hid box-testimonial light-color text-00 text-left">
+                                		<div class="quote-wrapper">
+                            				 From street food to Michelin star restaurants, my globetrotting exposed me to abundant cultures and cuisines. Find me frequently pondering unusual restaurant menus&mdash;or concocting novel recipes at home&mdash;and sharing with anyone who's willing (commemorative selfies while feasting are routine).<br><br>
+                                            Similarly, my career has followed a similar path&mdash;from humble math and neuroscience geek to working with innovative business thinkers in high tech and low tech IT/communications and security firms. Inevitably, I developed a penchant for enabling visionary ideas with diverse teams (best served over local food and beverage).
+                                            <br><br>
+                                            <span class="medium">Specialties</span>​<br>
+                                            Building global businesses and brands​<br>
+                                            Cultivating go-to-market strategies​<br>
+                                            Reengineering processes​<br>
+                                            Developing profitable revenue streams via direct and indirect sales channels​<br>
+                                            Creating demand and brand recognition​<br>
+                                            Measuring performance​<br>
+                                            Warming hearts and bellies​
+                                		</div>
+                                	</div>
+                                    <br><br>
                                 </div>
-                                <div class="col-md-3 col-sm-6">
+                                <div class="col-md-3 col-sm-6 iblock-multi">
                                     <img src="/images/headshot-mitch2.jpg" class="responsive">
                                     <p class="text regular">
                                         Mitch Cappelmann<br>
@@ -69,8 +86,23 @@
                                             GM, Software Engineering
                                         </span>
                                     </p>
+                                	<a class="bio-showhide noselect">bio</a>
+                                	<div class="hid box-testimonial light-color text-00 text-left">
+                                		<div class="quote-wrapper">
+                                            Like many in this industry, my love of technology started at an early age. I’d gotten a computer for my 14th birthday and soon after developed my first program&mdash;Alphabet Invaders. From that point on the only distraction from my interest in software development was traveling the world, which I did for many years after graduating from the College of Charleston.​<br><br>
+                                            When I eventually settled back down in the U.S., I found my home at several tech startups before spending five years at Microsoft as an architect and developer. This led to the creation of my own consulting company before joining the Bloom team.<br><br>
+                                            Of all of these experiences, however, there’s nothing I take pride in more than seeing my kids discover their own strengths and interests and guiding them along their individual paths.​
+                                            <br><br>
+                                            <span class="medium">Specialties</span>​<br>
+                                            Mobile/Phone​<br>
+                                            C#/XAML​<br>
+                                            Azure​<br>
+                                            User Experience​
+                                		</div>
+                                	</div>
+                                    <br><br>
                                 </div>
-                                <div class="col-md-3 col-sm-6">
+                                <div class="col-md-3 col-sm-6 iblock-multi">
                                     <img src="/images/headshot-andrew3.jpg" class="responsive">
                                     <p class="text regular">
                                         Andrew Herb<br>
@@ -78,8 +110,23 @@
                                             GM, Customer Experience
                                         </span>
                                     </p>
+                                	<a class="bio-showhide noselect">bio</a>
+                                	<div class="hid box-testimonial light-color text-00 text-left">
+                                		<div class="quote-wrapper">
+                                            My passion for making the complicated, simple, with technology has driven my career to some of the greatest places with more on the way. I have had the honor of working with incredibly talented people enabling visions to reality. I am always seeking talent to grow in this amazing time of opportunity.​
+ ​                                           <br><br>
+                                            With a Bachelor’s in Management information Systems from Oregon Institute of Technology, I've held many roles in the last two decades, working for industry leaders such as Compaq, Microsoft, Disney and T-Mobile. This has provided a wide range of experiences that help me better understand client needs and deliver flawless solutions.​
+                                             ​<br><br>
+                                            <span class="medium">Specialties</span>​<br>
+                                            Visionary of merging technology and business to drive superior results with a “WOW” factor​<br>
+                                            Inspiring, motivating, leading others to transform visions to reality​<br>
+                                            Problem solver…where there is a will, there is a way​<br>
+                                            Excellent smoke ring blower with a good cigar​
+                                		</div>
+                                	</div>
+                                    <br><br>
                                 </div>
-                                <div class="col-md-3 col-sm-6">
+                                <div class="col-md-3 col-sm-6 iblock-multi">
                                     <img src="/images/headshot-caleb2.jpg" class="responsive">
                                     <p class="text regular">
                                         Caleb Van Dyken<br>
@@ -87,6 +134,21 @@
                                             GM, Project Management Office
                                         </span>
                                     </p>
+                                	<a class="bio-showhide noselect">bio</a>
+                                	<div class="hid box-testimonial light-color text-00 text-left">
+                                		<div class="quote-wrapper">
+                                            As a former Combat Air Assault UH-60 Helicopter Pilot, I am accustomed to working well under pressure when the stakes are high. I thrive in environments where the requirements are vague, technology is new, risk is high, and timelines are short&mdash;like all cutting edge technological developments. I have been fortunate to lead small teams of highly skilled, steely eyed warriors as well as cross functional consulting teams to Fortune 50 companies. I take pride in bringing new products and services to market and am always on the lookout to optimize business processes to drive profitability.
+                                             ​<br><br>
+                                            <span class="medium">Specialties</span>​<br>
+                                            Agile Project Management​<br>
+                                            Change Management​<br>
+                                            Cross-functional Team Leadership​<br>
+                                            Program | Project Management​<br>
+                                            Business Development​<br>
+                                            Process Improvement​​
+                                		</div>
+                                	</div>
+                                    <br><br>
                                 </div>
                             </div>
                         </div>
@@ -100,7 +162,7 @@
             <section class="pt-medium pb-small light-color" data-nekoanim="fadeInUp" data-nekodelay="50">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6"><a name="culture" class="anchor"></a>
+                        <div class="col-md-6 iblock-multi"><a name="culture" class="anchor"></a>
                             <p class="text000 light">Culture</p>
                             <p class="text regular">
                                 Culture reflects the essence of Bloom because it directly affects the success of our strategy and hiring practices. 
@@ -133,11 +195,10 @@
             </section>
             <!-- / page content -->
 
-            <a name="news" class="anchor"></a>
             <section class="pt-small pb-medium light-color">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12"><a name="news" class="anchor"></a>
+                        <div class="col-md-12 iblock-multi"><a name="news" class="anchor"></a>
                             <p class="text000 light">Latest news <a href="/bloom.rss" target="_blank"><i class="material-icons icon">rss_feed</i></a></p>
                             <p class="text regular" data-nekoanim="fadeInLeft" data-nekodelay="10">
                                 <a href="http://www.securityinfowatch.com/article/12174810/bloom-consulting-delivers-bleeding-edge-tech-to-security-execs" target="_blank" class="text00">
@@ -172,6 +233,14 @@
 
 <?php include($base . "/part-scripts.php");?>
 
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $(".bio-showhide").on("click", function() {
+          $(this).next(".hid").fadeToggle();
+        });
+    });
+    </script>
+    
     <script type="text/javascript">
         $(document).ready(function() {
             $('#page-corporate').addClass('active');

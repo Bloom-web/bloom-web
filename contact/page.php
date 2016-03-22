cccb1cec64feb03b679215cab665d01615f4991a
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-03-22 22:15:04 +0000

    Reformated contact page, moved brochure file

--- contact/page.php
+++ contact/page.php
@@ -20,23 +20,52 @@
             <section class="">
                 <div class="">
                     <div class="row">
-                        <div class="col-sm-6 col-sm-push-6 pad-top-50" data-nekoanim="fadeInRight">
-                            <div class="text00">Contact us</div>
-                            <br>
-                            1-206-200-2231<br>
-                            information@bloomconsultinggroup.com<br>
-                            <br>
-                            8060 - 165th Ave. NE, Suite 200, Redmond, WA 98052<br>
-    						<br>
-    
-    						Offices:<br>
-                            Headquarters and Software Engineering - Redmond, WA (Suite 200-220)<br>
-                            Sales Offices - Redmond, WA + Washington D.C.<br>
-                            <br>
-                            <a href="https://s3-us-west-2.amazonaws.com/bcg-site-assets/Bloom_E_Brochure.pdf">Download a Bloom Consulting Group Brochure</a>
+                        <div class="col-sm-6 col-sm-push-6 pt-medium pb-medium text" data-nekoanim="fadeInRight">
+                            <div class="row pb-small">
+                                <div class="col-sm-1 iblock-multi tcell">
+                                    <i class="material-icons icon0">phonelink_ring</i>
+                                </div>
+                                <div class="col-sm-11 iblock-multi tcell text-left">
+                                    1-206-200-2231
+                                </div>
+                            </div>
+                            <div class="row pb-small">
+                                <div class="col-sm-1 iblock-multi tcell">
+                                    <i class="material-icons icon0">mail_outline</i>
+                                </div>
+                                <div class="col-sm-11 iblock-multi tcell text-left">
+                                    <a href="mailto: information@bloomconsultinggroup.com">information@bloomconsultinggroup.com</a>
+                                </div>
+                            </div>
+                            <div class="row pb-small">
+                                <div class="col-sm-1 iblock-multi tcell">
+                                    <i class="material-icons icon0">place</i>
+                                </div>
+                                <div class="col-sm-11 iblock-multi tcell text-left">
+                                    Headquarters (Suite 200) and Software Engineering (Suite 220)<br>
+                                    <a href="http://www.bing.com/mapspreview?&ty=18&q=8060%20165th%20Ave.%20NE%2c%20Redmond%2c%20WA%2098052&mb=47.681267~-122.129274~47.670272~-122.110777&tt=8060%20165th%20Ave%20NE%2c%20Redmond%2c%20WA%2098052&tsts0=%2526ty%253d18%2526q%253d8060%252520165th%252520Ave.%252520NE%25252c%252520Redmond%25252c%252520WA%25252098052%2526mb%253d47.681267~-122.129274~47.670272~-122.110777&tstt0=8060%20165th%20Ave%20NE%2c%20Redmond%2c%20WA%2098052&cp=47.676059~-122.123394&lvl=16&ftst=0&ftics=False&v=2&sV=1&form=S00027" target="_blank">
+                                    8060 165th Ave. NE, Redmond, WA 98052</a>
+                                </div>
+                            </div>
+                            <div class="row pb-small">
+                                <div class="col-sm-1 iblock-multi tcell">
+                                    <i class="material-icons icon0">attach_money</i>
+                                </div>
+                                <div class="col-sm-11 iblock-multi tcell text-left">
+                                    Sales Offices - Redmond, WA + Washington D.C.
+                                </div>
+                            </div>
+                            <div class="row pb-small">
+                                <div class="col-sm-1 iblock-multi tcell">
+                                    <i class="material-icons icon0">info_outline</i>
+                                </div>
+                                <div class="col-sm-11 iblock-multi tcell text-left">
+                                    <a href="/branding/Bloom_E_Brochure.pdf" target="_blank">Read our brochure</a>
+                                </div>
+                            </div>
                         </div>
                         <div class="col-sm-6 col-sm-pull-6" data-nekoanim="fadeInLeft">
-                            <div><iframe width="100%" height="400" frameborder="0" scrolling="no" seamless="seamless" src="http://www.bing.com/maps/embed/viewer.aspx?v=3&amp;cp=47.675770~-122.120026&amp;lvl=16&amp;w=500&amp;h=400&amp;sty=r&amp;typ=d&amp;pp=~~47.675770~-122.120026&amp;ps=&amp;dir=0&amp;mkt=en-us&amp;src=SHELL&amp;form=BMEMJS"></iframe></div>
+                            <iframe width="100%" height="500" frameborder="0" scrolling="no" seamless="seamless" src="http://www.bing.com/maps/embed/viewer.aspx?v=3&amp;cp=47.675770~-122.120026&amp;lvl=16&amp;w=600&amp;h=500&amp;sty=h&amp;typ=d&amp;pp=~~47.675770~-122.120026&amp;ps=&amp;dir=0&amp;mkt=en-us&amp;src=SHELL&amp;form=BMEMJS" style="vertical-align:bottom"></iframe>
                         </div>
 
                     </div>

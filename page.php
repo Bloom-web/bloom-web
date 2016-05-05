e324a0db18faec9546c108069388f872645fb722
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-05 22:20:33 +0000

    Perchified Home page

--- page.php
+++ page.php
@@ -17,14 +17,13 @@
                         <header class="page-header x-large stay image-background no-padding rel" style="background-image: url('/images/background-home.jpg');">
                             <div class="row">
                                 <div class="header-blurb blurb-home text0 regular col-md-7">
-                                    "Bloom innovation begins with a single, customer problem that causes pain, followed by a desire to change that outcome."
-                                    <br><span class="text-00 medium">Heather Zindel, C.E.O.</span>
+                                    <?php perch_content('blurb'); ?>
                                 </div>
                             </div>
                             <div class="slider-home bottom left right">
                                 <div class="owl-carousel neko-data-owl" data-neko_items="1" data-neko_singleitem="true" data-neko_pagination="true" data-neko_navigation="false" data-neko_transitionstyle="backSlide">
                                     <div class="item">
-                                        <div class="mask opacity-5">
+                                        <div class="mask opacity-7">
                                             <div class="flush">
                                                 <div class="row">
                                                     <div class="col-md-1"></div>
@@ -43,97 +42,13 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item">
-                                        <div class="image-background" style="background-image: url('/images/background-iscwest.jpg'); background-position: 0% 0%;">
-                                            <div class="row">
-                                                <div class="col-md-1"></div>
-                                                <div class="col-md-6 image-background" style="background-image: url('/images/background-iscwest-blur.jpg'); background-position: 0% 0%;">
-                                                    <div class="col-xs-4 iblock-mono">
-                                                        <div class="tcell">
-                                                            <img src="/images/logo-iscwest.png" class="slider-banner-half">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-xs-8 iblock-mono">
-                                                        <div class="tblock">
-                                                        <div class="tcell slider-banner-half text-left dark text000 light">
-                                                                We will be exhibiting our solutions this April 5-8 in Las Vegas, Nevada
-                                                        </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-5">
-                                                    <div class="tblock">
-                                                    <div class="tcell slider-banner-half">
-                                                        <a href="mailto:information@bloomconsultinggroup.com?subject=Visit Bloom at ISC West" class="btn default text00">Join us at ISC West</a>
-                                                    </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                                 <!-- corousel -->
                             </div>
                             <!-- slider home -->
                         </header>
                         <!-- page content -->
-                        <section class="pt-medium pb-medium light-color grayback">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-6" data-nekoanim="fadeInLeft" data-nekodelay="50">
-                                        <p class="text0000 light">Aggregating data and apps</p>
-                                        <p class="text0 regular">
-                                            Are employees suffering from information and app overload? Bloom aggregates enterprise web apps, LOB apps, social apps, and business intelligence into a single pane of glass.
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6" data-nekoanim="fadeInRight" data-nekodelay="50">
-                                        <img src="/images/app-overload.png" class="responsive">
-                                    </div>
-
-                                </div>
-                                <!-- row -->
-                            </div>
-                            <!-- container -->
-                        </section>
-                        <!-- / page content -->
-                        <section class="pt-medium pb-medium light-color">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-6" data-nekoanim="fadeInLeft" data-nekodelay="50">
-                                        <p class="text0000 light">Holographic computing</p>
-                                        <p class="text0 regular">
-                                            Bloom is a top Microsoft Windows Universal App development partner. With the release of the Hololens development kit, Bloom will be innovating once again to reimagine how security professionals collaborate and communicate in a mobile world.
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6" data-nekoanim="fadeInRight" data-nekodelay="50">
-                                        <img src="/images/hololens.jpg" class="responsive">
-                                    </div>
-
-                                </div>
-                                <!-- row -->
-                            </div>
-                            <!-- container -->
-                        </section>
-                        <!-- / page content -->
-                        <section class="pt-medium pb-medium light-color grayback">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-6" data-nekoanim="fadeInLeft" data-nekodelay="50">
-                                        <p class="text0000 light">IoT big data is here</p>
-                                        <p class="text0 regular">
-                                            The Internet of Things will grow to 26 billion units in 2020 according to Gartner, Inc. Due to decreasing costs, connectivity will become a standard feature of just about everything—leading to the need for big data cloud storage and data analytics. Ask sales executives about Bloom's current IoT project to analyze data from over 25 thousand physical security devices worldwide and surface it in its award winning Windows 10 Azure-driven app.
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6 text-center" data-nekoanim="fadeInRight" data-nekodelay="50">
-                                        <img src="/images/internetofthings.png" class="responsive">
-                                    </div>
-
-                                </div>
-                                <!-- row -->
-                            </div>
-                            <!-- container -->
-                        </section>
-                        <!-- / page content -->
+                        <?php perch_content('home topic'); ?>
 
                     </main>
                     <!-- / content -->

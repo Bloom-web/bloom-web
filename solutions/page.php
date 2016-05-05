e4f741b8774b70acd8fff7182632b01c3471a7dd
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-05 22:21:28 +0000

    Perchified Solutions page

--- solutions/page.php
+++ solutions/page.php
@@ -2,8 +2,7 @@
 <?php include($base . '/perch/runtime.php');?>
 <?php include($base . "/part-doctype.php");?>
     <title>Bloom - Solutions</title>
-    <meta name="description" content="">
-    <meta name="author" content="Bloom">
+<?php perch_content('meta tags'); ?>
 
 <?php include($base . "/part-styles.php");?>
 
@@ -19,8 +18,7 @@
                 <div class="header-shade"></div>
                 <div class="row">
                     <div class="header-blurb blurb-solutions text000 regular col-md-7">
-                        "Experience is everything. What we deliver doesn't matter if the customer experience is less than flawless."<br>
-                        <span class="text-00 medium">Andrew Herb, GM of Client Experience</span>
+                        <?php perch_content('blurb'); ?>
                     </div>
                 </div>
             </header>
@@ -44,82 +42,11 @@
 			<!-- / video -->
 
             <!-- page content -->
-            
-            <section class="pt-medium light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7" data-nekoanim="fadeInLeft" data-nekodelay="10"><a name="security" class="anchor"></a>
-                            <p class="text0000 light">Modern Enterprise Security Solutions</p>
-                            <p class="text0 regular">
-                                Chief Security Officers and their directs suffer from App and information overload possibly more than any other group in the enterprise. The enterprise security executive is responsible for risk, physical security, and people safety, relying on a myriad of point solutions and data points to manage their business. Bloom helps the modern CSO and Security Executive transform their security operations to achieve much better line of sight across the organization. 
-                                <br><br>
-                                Bloom Modern CSO Dashboard&trade;<br>
-                                Bloom Modern Incident Management Virtual Team Fusion Experience&trade;<br>
-                                Bloom Modern Security Operations Apportal&trade;<br>
-                                Bloom Modern Physical Security IoT Dashboard&trade;<br>
-                                Bloom Modern PSIM App&trade;<br>
-                                Bloom Modern Holographic Virtual Collaboration&trade;<br>
-                                Bloom Modern Big Data Security Analytics&trade;<br>
-                            </p>
-                        </div>
-                        <div class="col-md-5 pad-top-75 text-center" data-nekoanim="fadeInRight" data-nekodelay="10">
-                            <img src="/images/modern-security.jpg" class="responsive">
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- / page content -->
-            
-            <section class="pt-medium light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-md-push-5" data-nekoanim="fadeInRight" data-nekodelay="10"><a name="sales" class="anchor"></a>
-                            <p class="text0000 light">Modern Enterprise Sales Solutions</p>
-                            <p class="text0 regular">
-                                Sales people are highly mobile and require modern, mobile experience that deliver information to them seamlessly across their many devices. Bloom's modern sales solutions integrate sales apps into a single pane of glass view for continuous workflow and mobility of experience. Imagine having your CRM pipeline and sales data, sales collateral, training books, and customer presentations at your fingertips. Imagine everything you need in a single application that can be shared with customers through customer-centric views. Bloom can help sales professionals transform their sales organizations by increasing productivity to achieve much better ROI and differentiate competitively with customers.
-                                <br><br>
-                                Bloom Modern Interactive Sales Customer Demo&trade;<br>
-                                Bloom Modern Sales Apportal&trade;<br>
-                                Bloom Modern Sales BI Dashboard&trade;<br>
-                                Bloom Modern Field Training App&trade;<br>
-                                Bloom Modern Holographic Virtual Collaboration&trade;<br>
-                                Bloom Modern Big Data Marketing Analytics&trade;<br>
-                            </p>
-                        </div>
-                        <div class="col-md-5 col-md-pull-7 pad-top-75 text-center" data-nekoanim="fadeInLeft" data-nekodelay="10">
-                            <img src="/images/modern-sales.jpg" class="responsive">
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- / page content -->
-            
-            <section class="pt-medium pb-medium light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7" data-nekoanim="fadeInLeft" data-nekodelay="10"><a name="marketing" class="anchor"></a>
-                            <p class="text0000 light">Modern Enterprise Marketing Solutions</p>
-                            <p class="text0 regular">
-                                A few of the mega trends impacting marketers today are at the center of the Bloom solution suite. Mobile is going to continue to be the center of marketing. From cell phones to smartphones, tablets to wearable gadgets, the evolution of mobile devices is one of the prime factors influencing marketing executives messaging to their customer audience. Information overload will continue. Social will evolve to be the channel for marketers. Personalized, data-driven marketing will continue to dominate big data and BI investments and more accurate telemetry to measure engagement will surface. 
-                                <br><br>
-                                Bloom Modern Mobile Maturity (M3) Assessment&trade;<br>
-                                Bloom Interactive Tradeshow App&trade;<br>
-                                Bloom Marketing Apportal&trade;<br>
-                                Bloom Newsletter App&trade;<br>
-                                Bloom Marketing Brochure App&trade;<br>
-                                Bloom Field Training App&trade;<br>
-                                Bloom Holographic Virtual Collaboration&trade;<br>
-                                Bloom Big Data Marketing Analytics&trade;<br>
-                            </p>
-                        </div>
-                        <div class="col-md-5 pad-top-75 text-center" data-nekoanim="fadeInRight" data-nekodelay="10">
-                            <img src="/images/modern-marketing.jpg" class="responsive">
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- / page content -->
+            <?php perch_content('solutions'); ?>
 
+            <!-- extra space -->
+            <section class="pt-medium light-color">
+            </section>
         </main>
         <!-- / content -->
 

f36a3e5faabd76775ff8c34f8a454c676d84f058
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-05 22:21:47 +0000

    Perchified Services page

--- services/page.php
+++ services/page.php
@@ -2,8 +2,7 @@
 <?php include($base . '/perch/runtime.php');?>
 <?php include($base . "/part-doctype.php");?>
     <title>Bloom - Services</title>
-    <meta name="description" content="">
-    <meta name="author" content="Bloom">
+<?php perch_content('meta tags'); ?>
 
 <?php include($base . "/part-styles.php");?>
 
@@ -18,8 +17,7 @@
             <header class="page-header no-padding rel">
                 <div class="row">
                     <div class="header-blurb blurb-services text00 regular col-md-5">
-                        "When you think there is nothing left to improve on, your business dies, for there is no shortage of innovators."<br>
-                        <span class="text-00 medium">Bankgambiki Habyarimana, The Great Pearl of Wisdom</span>
+                        <?php perch_content('blurb'); ?>
                     </div>
                 </div>
             </header>
@@ -43,157 +41,12 @@
 
             <!-- page content -->
             <section class="pt-medium light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="consulting" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">trending_up</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Strategic consulting</p>
-                                <p class="text0 regular">
-                                    Bloom Consultants help organizations innovate, transform, and modernize. They act as facilitators to problem-solve and provide insight using a best practice approach to help clients reach their business objectives. What does a facilitator do? The word comes from the Latin word for “easy” implying that we make the work of our clients easier. Our clients benefit from decades of leadership in marketing, sales, security and partner program experience.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="analytics" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">wifi_tethering</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">IoT and big data analytics</p>
-                                <p class="text0 regular">
-                                    Connectivity will become a standard feature of just about everything, leading to the need for big data cloud storage and analytics to uncover new insights. Bloom can integrate existing business information with new machine-generated data to enhance decision making. Analytics is not just for thousands of devices and sensors in the future, it's what you use today to derive business value.
-                                </p>
-                            </div>
-                        </div>
-                    </div> <!-- / row -->
-                </div><!-- / container -->
-            </section>
-            <section class="pt-small light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="appdev" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">view_module</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Windows application development</p>
-                                <p class="text0 regular">
-                                    Bloom is one of the leading Microsoft Windows 10 Application developers creating follow-you-anywhere experiences across Windows-based devices. Bloom can also create Xamarin cross-platform projects that deliver a seamless application experience across iOS and Android-based devices.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="experience" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">tag_faces</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">User experience and interface design</p>
-                                <p class="text0 regular">
-                                    It's easy to get so pumped up about technology and the function of software that the end user is forgotten. The reality is that although we work hard to make the applications we build work, the user just expects them to work. After that, they want their experience to be effortless and seamless across devices. Bloom's world class experience architects work tirelessly to ensure the UI (refers to the visuals and controls of your application) and the user experience (encompasses both the UI and the behavior of the application related to the UI as well as the "feeling" the user gets from the app) is awesome.
-                                </p>
-                            </div>
-                        </div>
-                    </div> <!-- / row -->
-                </div><!-- / container -->
-            </section>
-            <section class="pt-small light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="performance" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">check_circle</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Business performance improvement</p>
-                                <p class="text0 regular">
-                                    Just as a car needs a tune-up every once in a while to perform at its best, business practices also need to be taken apart and tweaked in order to stay in top shape. Consultants at Bloom have the experience and tools to turn your area of business into a top performer.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="holographic" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">3d_rotation</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Holographic computing experiences</p>
-                                <p class="text0 regular">
-                                    As part of the Microsoft Windows Insiders Program and a leading Windows 10 application development partner, Bloom will soon be innovating for business with the HoloLens development Kit. In late Q2, Bloom will be announcing new solutions to collaborate virtually.
-                                </p>
-                            </div>
-                        </div>
-                    </div> <!-- / row -->
-                </div><!-- / container -->
             </section>
             
-            <section class="pt-small light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="intelligence" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">insert_chart</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Business intelligence</p>
-                                <p class="text0 regular">
-                                    Bloom helps clients find ways to turn everyday information into actionable insights by embedding analytics across the organization. Bloom can create custom Power BI or app dashboards, Excel reports, scorecards, and perform complex data integration.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="cloud" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">cloud_queue</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Azure cloud services</p>
-                                <p class="text0 regular">
-                                    In a time when capacity is king, Microsoft Azure allows for endless expansion of data without the expense and hassle called for by on premises solutions. Azure Cloud Services is an important tool that Bloom utilizes for our clients and ourselves to simplify processes, cut costs, and allow for the highest performance.
-                                </p>
-                            </div>
-                        </div>
-                    </div> <!-- / row -->
-                </div><!-- / container -->
+            <?php perch_content('services'); ?>
+
+            <section class="pt-medium light-color">
             </section>
-            <section class="pt-small pb-medium light-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="outsourcing" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">assignment</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Project management outsourcing</p>
-                                <p class="text0 regular">
-                                    Bloom Project Managers have deep experience leading and managing complex initiatives to achieve project milestones and objectives. Specifically, they can help you with project initiation and planning, outlining scope, managing resources, monitoring project quality, managing risk, and measuring performance, so your project and programs come in on time and within budget.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6" data-nekoanim="fadeInUp">
-                            <a name="transformation" class="anchor"></a>
-                            <div class="col-sm-2 text-center">
-                                <i class="material-icons icon0000">stars</i>
-                            </div>
-                            <div class="col-sm-10">
-                                <p class="text0000 light">Project management transformation</p>
-                                <p class="text0 regular">
-                                    Our Project Managers have a wealth of knowledge, not only based on experience, but based on well known, universal methodologies that are proven to be successful if used correctly. From Agile to Waterfall, we can analyze your needs, recommend the best course of action, and facilitate the development of a world class project management program. Our PMP and Scrum Master-certified professionals have transformed large enterprise organizations project management practices from good to great.
-                                </p>
-                            </div>
-                        </div>
-                    </div> <!-- / row -->
-                </div><!-- / container -->
-            </section>
-            
             
         </main>
         <!-- / content -->

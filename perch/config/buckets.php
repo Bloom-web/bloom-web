4e947d50ba74bccb000bfca8c3898a5f0c5eddb4
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-06 05:11:22 +0000

    Perchified Clients page (figured out logic for detecting rows)

--- perch/config/buckets.php
+++ perch/config/buckets.php
@@ -8,6 +8,10 @@
 			'web_path'  => '/imagez/screenshots',
 			'file_path' => realpath(PERCH_RESFILEPATH.DIRECTORY_SEPARATOR.'screenshots'),
 		),
+		'pdfs' => array(
+			'web_path'  => '/pdfs',
+			'file_path' => realpath($_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR.'pdfs'),
+		),
 		'backgrounds' => array(
 			'web_path'  => '/imagez/backgrounds',
 			'file_path' => realpath(PERCH_RESFILEPATH.DIRECTORY_SEPARATOR.'backgrounds'),

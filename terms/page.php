8dfe93dcea6e73df820cfcd422e965ba48934c73
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-06 07:48:46 +0000

    Perchified Terms and Privacy page

--- terms/page.php
+++ terms/page.php
@@ -1,107 +1,50 @@
 <?php $base = $_SERVER["DOCUMENT_ROOT"]; ?>
 <?php include($base . '/perch/runtime.php');?>
 <?php include($base . "/part-doctype.php");?>
-        <title>Bloom - Privacy and Terms</title>
-        <meta name="description" content="">
-        <meta name="author" content="Bloom">
+<title>Bloom - Privacy and Terms</title>
+<meta name="description" content="">
+<meta name="author" content="Bloom">
 
-        <?php include($base . "/part-styles.php");?>
+<?php include($base . "/part-styles.php");?>
 
-            <body class="activate-appear-animation header-6 pre-header-on-sm">
-                <!-- global-wrapper -->
-                <div id="global-wrapper">
-
-                    <?php include($base . "/part-navigation.php");?>
-                        <!-- content-->
-
-                        <main id="content">
-                            <header class="page-header xlarge image-background no-padding rel" style="background-image: url('/images/background-news.jpg');">
-                            </header>
-
-                            <!-- page content -->
-
-                            <section class="pt-medium pb-medium light-color">
-                                <div class="container">
-
-                                    <div class="text0000 light">Terms</div>
-                                    <div class="lumps">
-                                        <p class="text-0">If you continue to browse and use this website or app, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Bloom Consulting Group's relationship with you in relation to this website or app. If you disagree with any part of these terms and conditions, please do not use our website or app.</p>
-
-                                        <p class="text-0">The term 'Bloom Consulting Group' or 'us' or 'we' refers to the owner of the website or app whose registered office is 8060 165th Ave NE, Suite 200, Redmond, WA 98052. The term 'you' refers to the user or viewer.</p>
-
-                                        <div class="text-0">The use of this website or app is subject to the following terms of use:
-                                            <br>
-                                            <ul>
-                                                <li>The content of the pages is for your general information and use only. It is subject to change without notice.</li>
-
-                                                <li>Our website or app may use cookies to monitor browsing preferences.</li>
-
-                                                <li>Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website or app for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</li>
-
-                                                <li>Your use of any information or materials on this website or app is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website or app meet your specific requirements.</li>
-
-                                                <li>This website or app contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
-
-                                                <li>All trademarks, service marks, trade names, trade dress, product names and logos appearing on the site are the property of their respective owners, including in some instances Bloom Consulting Group. Any rights not expressly granted herein are reserved. Such trademarks may include examples such as: Microsoft<sup>&reg;</sup>, Hololens<sup>&trade;</sup>, and Azure<sup>&trade;</sup> are trademarks of Microsoft Corporation.</li>
-
-                                                <li>Unauthorized use of this website or app may give rise to a claim for damages and/or be a criminal offence.</li>
-
-                                                <li>From time to time this website or app may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s) or app(s). We have no responsibility for the content of the linked website(s) or app(s).</li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                    <div class="pt-medium text0000 light">Privacy Statement</div>
-                                    <div class="lumps">
-                                        <p class="text-0">This privacy policy sets out how Bloom Consulting Group uses and protects any information on their website or app.</p>
-
-                                        <p class="text-0">Bloom Consulting Group is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website or app, then you can be assured that it will only be used in accordance with this privacy statement.</p>
-
-                                        <p class="text-0">Bloom Consulting Group may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from 2016-03-01.</p>
-
-                                        <div class="text000 light">Security</div>
-
-                                        <p class="text-0">We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</p>
-
-                                        <div class="text000 light">How we use cookies</div>
-
-                                        <p class="text-0">A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree, the file is added and the cookie helps analyze web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences.</p>
-
-                                        <p class="text-0">We may use traffic log cookies to identify which pages are being used. This helps us analyze data about webpage traffic and improve our website or app in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.</p>
-
-                                        <p class="text-0">Overall, cookies help us provide you with a better website or app by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.</p>
-
-                                        <p class="text-0">You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website or app.</p>
-
-                                        <div class="text000 light">Links to other websites or apps</div>
-
-                                        <p class="text-0">Our website or app may contain links to other websites of interest. However, once you have used these links to leave our site, you should note that we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the website or app in question.</p>
-
-                                        <div class="text000 light">Disclaimer</div>
-
-                                        <p class="text-0">The information contained in this website or app is for general information purposes only. The information is provided by Bloom Consulting Group and while we endeavor to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website/app or the information, products, services, or related graphics contained on the website or app for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
-
-                                        <p class="text-0">In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website or app.</p>
-
-                                        <p class="text-0">Through this website or app you are able to link to other websites which are not under the control of Bloom Consulting Group. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.</p>
-
-                                        <p class="text-0">Every effort is made to keep the website or app up and running smoothly. However, Bloom Consulting Group takes no responsibility for, and will not be liable for, the website or app being temporarily unavailable due to technical issues beyond our control.</p>
-
-                                    </div>
-                                    <!-- / row -->
-                                </div>
-                                <!-- / container -->
-                            </section>
-
-                        </main>
-                        <!-- / content -->
-
-                        <?php include($base . "/part-footer.php");?>
+<body class="activate-appear-animation header-6 pre-header-on-sm">
+<!-- global-wrapper -->
+        <div id="global-wrapper">
+        
+        <?php include($base . "/part-navigation.php");?>
+        <!-- content-->
+        
+        <main id="content">
+            <header class="page-header xlarge image-background no-padding rel" style="background-image: url('/images/background-news.jpg');">
+            </header>
+        
+            <!-- page content -->
+        
+            <section class="pt-medium pb-medium light-color">
+                <div class="container">
+        
+                    <div class="text0000 light">Terms</div>
+                    <div class="text-0 lumps">
+                    <?php perch_content('terms');?>        
+                    </div>
+                    <div class="pt-medium text0000 light">Privacy Statement</div>
+                    <div class="text-0 lumps">
+                    <?php perch_content('privacy');?>
+                    </div>
+                    <!-- / row -->
                 </div>
-                <!-- global wrapper -->
+                <!-- / container -->
+            </section>
+        
+        </main>
+        <!-- / content -->
+        
+        <?php include($base . "/part-footer.php");?>
+        </div>
+        <!-- global wrapper -->
 
-                <?php include($base . "/part-scripts.php");?>
+<?php include($base . "/part-scripts.php");?>
 
-            </body>
+</body>
 
-            </html>
+</html>

7516b88ad07b3b2cb9c710e4b67ab5b0a9258344
Bloom Design Staging <design@bloomconsultinggroup.com>
2016-05-06 06:31:52 +0000

    Perchified Careers page

--- careers/page.php
+++ careers/page.php
@@ -21,17 +21,7 @@
                     <div class="header-blurb blurb-services text00 light col-md-4">
                         <div class="text0 pad-bottom-20">Employee Testimonials</div>
                         <div class="owl-carousel neko-data-owl" data-neko_items="1" data-neko_singleitem="true" data-neko_pagination="false" data-neko_navigation="false" data-neko_autoPlay="10000" data-neko_transitionstyle="backSlide">
-                            <div class="item">
-                            "One of the best things about working for Bloom is that they encourage creativity and innovation, so you can put your own spin on things while constantly being challenged to do better"
-    						<span class="text-00 medium"><br><br>
-    						- Cassie Fischer, Project Manager</span>
-                            </div>
-                            <div class="item">
-                            "Bloom is a highly collaborative work environment. Each deliverable has the input of multiple team members, working together to create our signature gold standard products"
-                            <span class="text-00 medium"><br><br>
-                            - Sukhie Patel, Project Manager</span>
-                            </div>
-                        </div>
+                        <?php perch_content('quotes');?>
                     </div>
                 </div>
             </header>
@@ -44,121 +34,7 @@
                 </div>
             </section>
 
-            <section class="pt-small pb-medium light-color" data-nekoanim="fadeInUp">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p class="text000 light">Microsoft-Experienced Project Manager</p>
-                            <div class="text regular">
-                                We are looking for an experienced Microsoft Project Manager who will take on the management of key client marketing, sales, and channel projects with the goal of delivering every project on time, within budget and within scope.
-                            </div>
-                            <a class="showhide noselect text">Description <i class="material-icons aligned">expand_more</i></a>
-                            <div class="hid text regular">
-                                If you have been working with Microsoft at a vendor firm and want to be part of a growing consulting and technology firm that works for a team and provides salary, bonus, and full benefits as part of our compensation package regardless of whether you’re on the bench, our firm is for you. Tired of being “off-boarded” when your contract ends? Tired of working as an individual PM versus as part of a team? Frustrated that you have so much to contribute but no-one will hire you because you don’t have a v-dash? Talk to our PMO Lead to discuss how we can help you grow your career. Our PMO is growing, is team-oriented, and working with several technology clients, not just Microsoft. Excellent opportunities for growth, career advancement and cross training with our Software Development team. Established relationships at Microsoft SMS&P, EPG, M&O, and/or the BGs a plus, with opportunities for sales incentives included. Interested in contracting with your current client under our firm’s brand with resources and tools to support you, that’s okay too.
-                                <br><br>
-                                <div class="text-0">
-                                    Responsibilities
-                                    <ul>
-                                        <li>Define and review project scope, goals, and deliverables that support business goals as stated in key agreements (e.g. Statement of Work) in collaboration with senior management objectives</li>
-    
-                                    <li>Ensure that all projects are delivered on-time, within scope and within budget</li>
-                                    
-                                    <li>Assist in the definition of project scope and objectives, involving all relevant stakeholders and ensuring technical feasibility</li>
-                                    
-                                    <li>Develop a detailed project plan to monitor and track progress</li>
-                                    
-                                    <li>Manage changes to the project scope, project schedule, and project costs using appropriate verification techniques</li>
-                                    
-                                    <li>Measure project performance using appropriate tools and techniques</li>
-                                    
-                                    <li>Report and escalate to management as needed</li>
-                                    
-                                    <li>Successfully manage the relationship with the client and all stakeholders</li>
-                                    
-                                    <li>Perform risk management to minimize project risks</li>
-                                    
-                                    <li>Establish and maintain relationships with third parties/vendors</li>
-                                    
-                                    <li>Create and maintain comprehensive project documentation</li>
-                                    
-                                    <li>Write and shape key deliverables for the client and aligned to the consulting team</li>
-                                    </ul>
-                                    Requirements
-                                    <ul>
-                                    <li>Bachelor’s degree required, preferably in the fields of computer science, engineering, or business</li>
-                                    
-                                    <li>Proven working experience in project management in the information technology sector</li>
-                                    
-                                    <li>Solid technical background with understanding and/or hands-on experience in software development and web technologies</li>
-                                    
-                                    <li>Excellent client-facing and internal communication skills</li>
-                                    
-                                    <li>Excellent written and verbal communication skills</li>
-                                    
-                                    <li>Solid organizational skills including attention to detail and multi-tasking skills</li>
-                                    
-                                    <li>Strong working knowledge of Microsoft Office</li>
-                                    
-                                    <li>PMP certification is a plus</li>
-                                    
-                                    <li>CSM certification is a plus
-                                    </ul>
-                                    Salary: Negotiable depending on experience.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 pad-top-75 text-center">
-                            <a href="mailto:recruiting@bloomconsultinggroup.com?subject=Project Manager" class="btn default large block text0000">apply now</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="pt-small pb-medium light-color" data-nekoanim="fadeInUp">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p class="text000 light">Windows Developer</p>
-                            <div class="text regular">
-                                Skilled software developer passionate about highly interactive multi-tiered mobile applications. Windows store and windows phone experience a plus. Thrives in a fast-paced environment and able to manage ambiguity. Motivated self-starter who brings solutions to the table. Comfortable in a collaborative team environment.
-                            </div>
-                        </div>
-                        <div class="col-md-4 pad-top-75 text-center">
-                            <a href="mailto:recruiting@bloomconsultinggroup.com?subject=Windows Developer" class="btn default large block text0000">apply now</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="pb-medium light-color" data-nekoanim="fadeInUp">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p class="text000 light">Cloud/Enterprise Solution Architect</p>
-                            <div class="text regular">
-                                The Solution Architect is a key member of the company’s custom solution implementation project team and is responsible for the overall design of the application and mapping client business requirements to systems/technical requirements. The Solution Architect works as a team member along with the Customer Representative, Product Owner and supporting team members to deliver the complete solution for the customer. This is a hands-on position.
-                            </div>
-                        </div>
-                        <div class="col-md-4 pad-top-75 text-center">
-                            <a href="mailto:recruiting@bloomconsultinggroup.com?subject=Cloud/Enterprise Solution Architect" class="btn default large block text0000">apply now</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="pb-medium light-color" data-nekoanim="fadeInUp">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p class="text000 light">Database Admin/Business Analyst</p>
-                            <div class="text regular">
-                                The Database Admin/BusinessAnalyst will be responsible for the maintenance of existing databases, the design, and implementation of new databases. This person will also be responsible for analyzing our current systems, resolving data reporting issues and making necessary improvements. We are looking for someone that can work with our existing developers and key team members to understand needs and expectations and offer solutions.
-                            </div>
-                        </div>
-                        <div class="col-md-4 pad-top-75 text-center">
-                            <a href="mailto:recruiting@bloomconsultinggroup.com?subject=Database Admin/Business Analyst" class="btn default large block text0000">apply now</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+            <?php perch_content('open positions');?>
 
         </main>
         <!-- / content -->
